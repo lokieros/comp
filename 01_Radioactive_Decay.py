937018f77cebe3c5f95977dcd8666979fc9d7b0d
vicospacorum <fdomingos@usp.br>
2018-04-15 01:34:39 -0300

    Add base file

---
+++ 01_Radioactive_Decay.py
@@ -0,0 +1,12 @@
+# -*- coding: cp1252 -*- 
+# Simulation of Radioactive Decay
+# Based on a True Basic program by N. Giordano (10-1-94)
+# Adapted from "Computational Physics" by N. Giordano, 1st Edition, Chapter 1
+
+from vpython import * 
+
+def main():
+	
+
+if __name__ == '__main__':
+    main()
